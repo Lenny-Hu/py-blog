d107cfa76e327e13d5e5d462f0398e268ed5c641
胡乐 <huyue@Lenny-mpc.local>
2018-08-12 14:22:19 +0800

    发布标签和下载标签命令

--- fabfile.py
+++ fabfile.py
@@ -13,7 +13,7 @@ def hello():
 @task
 def install(requirements_env='dev'):
     """安装所有依赖"""
-    local('pip3 install -r requirements/%s.txt' % requirements_env);
+    local("pip3 install -r requirements/%s.txt" % requirements_env);
 
 @task
 # 运行服务器
@@ -26,3 +26,16 @@ def runserver():
 def pep8():
     """使用pep8检查代码"""
     local('pep8 .');
+
+@task
+# 创建标签并发布到GitHub上
+def tag_version(version):
+    """创建标签并发布到GitHub上"""
+    local("git tag %s" % version);
+    local("git push origin %s"%version);
+
+@task
+# 下载GitHub上指定版本的代码
+def fetch_version(version):
+    """从github上下载指定tag的代码"""
+    local("wget https://github.com/Lenny-Hu/py-blog/archive/%s.tar.gz"%version);
